943dfaf147c9fd9a73147b175f28a8a69c908238
Aura Cho <acho1833@inst.hcpss.org>
2025-06-10 01:36:18 +0000

    commit

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,10 @@
+import { Card } from "@/components/ui/card";
+import { SignUpView } from "@/modules/auth/ui/views/sign-up-in-view";
+
+const Page = () => {
+    return (
+        <SignUpView />
+    );
+}
+
+export default Page;
