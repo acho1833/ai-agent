93ff4e2c81923f1ce4a5ae685ef16372b277c464
Aura Cho <acho1833@inst.hcpss.org>
2025-06-19 20:35:22 +0000

    commit

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000",
+    baseURL: "https://reimagined-guacamole-wr97wp6r5r97396vx-3000.app.github.dev",
 })
